864560ae4a3c06c5a79acfe8001cc82092147fc9
todor147 <todor_147@abv.bg>
2024-10-24 12:42:25 +0100

    Navbar work

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { HiOutlineBars3CenterLeft } from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { CiHeart } from "react-icons/ci";
 
 const Navbar = () => {
+  const currentUser = false;
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
@@ -14,7 +18,7 @@ const Navbar = () => {
           </Link>
           {/*search input */}
           <div className="relative sm:w-72 w-40 space-x-1">
-            <IoSearchOutline className="absolute inline-block left-3 inset-2"/>
+            <IoSearchOutline className="absolute inline-block left-3 inset-2" />
             <input
               type="text"
               placeholder="Search here"
@@ -23,7 +27,31 @@ const Navbar = () => {
           </div>
         </div>
         {/*right side */}
-        <div>nav items</div>
+        <div className="relative flex items-centre md:space-x-3 space-x-2">
+          <div>
+            {currentUser ? (
+              <>
+                <button>
+                  <img src="" alt="" />
+                </button>
+              </>
+            ) : (
+              <Link to="/login">
+                <FaUser className="size-6" />
+              </Link>
+            )}
+          </div>
+          <button className="hidden sm:block">
+            <CiHeart className="size-6" />
+          </button>
+          <Link
+            to="/cart"
+            className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm"
+          >
+            <MdOutlineShoppingCart className="" />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
       </nav>
     </header>
   );
